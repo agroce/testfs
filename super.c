1e7ba92112e35dd3445966bca05067c45fc17dad
Alex Groce <agroce@gmail.com>
2018-07-30 14:37:52 -0700

    discard const

--- super.c
+++ super.c
@@ -76,7 +76,7 @@ testfs_init_super_block(const char* storage, int corrupt, struct super_block **s
                 return -ENOMEM;
         }
 
-	sb->storage = storage;
+	sb->storage = (char*)storage;
 
         read_blocks(sb, block, 0, 1);
         memcpy(&sb->sb, block, sizeof(struct dsuper_block));
