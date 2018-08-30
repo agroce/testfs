7e375b9b0c9545e3daf0efe1263687ee4a4cfadd
Alex Groce <agroce@gmail.com>
2018-08-30 13:11:36 -0700

    more verbosity on resets

--- block.c
+++ block.c
@@ -18,7 +18,9 @@ write_blocks(struct super_block *sb, char *blocks, int start, int nr)
 {
   if (reset_countdown > 0) {
     reset_countdown -= 1;
+    printf ("Decremented reset counter to %d", reset_countdown);
   } else if (reset_countdown == 0) {
+    printf ("Ignoring write due to simulated reset");    
     return;
   }
   memcpy(sb->storage + (start * BLOCK_SIZE), blocks, nr * BLOCK_SIZE);
