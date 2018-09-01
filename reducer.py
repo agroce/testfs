827d9a47aff024794527d4a4c69944e92729ac8b
Alex Groce <agroce@gmail.com>
2018-09-01 12:57:50 -0700

    fix types

--- reducer.py
+++ reducer.py
@@ -88,7 +88,7 @@ while changed:
 
 if (s[1] + 1) > len(currentTest):
     print "PADDING TEST WITH ZEROS"
-    padding = bytearray('\x00' * (s[1] + 1) - len(currentTest))
+    padding = bytearray('\x00' * ((s[1] + 1) - len(currentTest)))
     currentTest = currentTest + padding
         
 with open(out, 'wb') as outf:
