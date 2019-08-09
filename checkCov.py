071ed085c17d453a5e7d98b1e8259182513f05c6
agroce <agroce@gmail.com>
2019-08-09 10:17:06 -0700

    coverage script

---
+++ checkCov.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+import os
+
+os.remove('default.profraw')
+
+subprocess.call(["./TestsCov --input_test_file " + sys.argv[1]], shell=True)
+subprocess.call(["llvm-profdata-6.0 merge -o testscov.profdata default.profraw"], shell=True)
+subprocess.call(["llvm-cov-6.0 show ./TestsCov -instr-profile=testscov.profdata *.c Tests.cpp  >& new.covout"], shell=True)
+
+coverage_original = []
+with open("covout", 'r') as covf:
+    for line in covf:
+        ls = line.split("|")
+        try:
+            count = int(ls[1])
+            if count > 1:
+                count = 1
+        except:
+            count = -1
+        coverage_original.append(count)
+
+coverage_new = []
+with open("new.covout", 'r') as covf:
+    for line in covf:
+        ls = line.split("|")
+        try:
+            count = int(ls[1])
+            if count > 1:
+                count = 1
+        except:
+            count = -1
+        coverage_new.append(count)
+
+if coverage_original == coverage_new:
+    sys.exit(0)
+else:
+    sys.exit(1)
