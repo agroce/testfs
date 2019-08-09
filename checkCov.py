49d491666dac60db958dd1117f7a7903626df7a2
agroce <agroce@gmail.com>
2019-08-09 10:28:34 -0700

    show structure

--- checkCov.py
+++ checkCov.py
@@ -5,7 +5,7 @@ import os
 
 os.remove('default.profraw')
 
-subprocess.call(["./TestsCov --input_test_file " + sys.argv[1]], shell=True)
+subprocess.call(["./TestsCov --verbose_reads --input_test_file " + sys.argv[1]], shell=True)
 subprocess.call(["llvm-profdata-6.0 merge -o testscov.profdata default.profraw"], shell=True)
 with open("new.covout", 'w') as newcovf:
     subprocess.call(["llvm-cov-6.0 show ./TestsCov -instr-profile=testscov.profdata *.c Tests.cpp"], shell=True, stdout=newcovf,
